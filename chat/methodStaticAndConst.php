09efc62c9cf729ef40351f3f8d5492bb99bd5383
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2025-06-17 08:42:20 -0300

    feat: More example for a static

--- chat/methodStaticAndConst.php
+++ chat/methodStaticAndConst.php
@@ -6,4 +6,14 @@
             return $a + $b;
         }
     }
+
+    class Until {
+        public static $contador = 0;
+        
+        public static function somar ($a, $b) {
+            return $a + $b;
+        }        
+    }
+
+    echo Until::somar(5, 3); // 8
 ?>
